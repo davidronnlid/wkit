e3cb24f7a36b9e2ecca10654a02a51c9f4fe8bfe
David Rönnlid <david.ronnlid@gmail.com>
2022-10-16 20:58:49 +0200

    Init push

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import David from "./images/david.png";
+import { ReactComponent as WKIT } from "./images/wkit.svg";
+import "./App.css";
+import { Link, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/homePage";
+import PageTwo from "./pages/pageTwo";
+import PageThree from "./pages/pageThree";
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
 
-function App() {
+const App = (): JSX.Element => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <header>
+        <ButtonGroup
+          variant="contained"
+          aria-label="outlined primary button group"
         >
-          Learn React
-        </a>
+          <Button>
+            <Link to="/">Home</Link>
+          </Button>
+          <Button>
+            <Link to="/pageTwo">Page two</Link>
+          </Button>
+          <Button>
+            <Link to="/pageThree">Page three</Link>
+          </Button>
+        </ButtonGroup>
       </header>
-    </div>
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/pageTwo" element={<PageTwo />} />
+        <Route path="/pageThree" element={<PageThree />} />
+      </Routes>
+      <footer>
+        <img src={David} alt="David smiling" style={{ width: "25vw" }} />
+        <span style={{ fontSize: "6vw", paddingBottom: "6vw" }}>💚</span>
+        <WKIT
+          style={{
+            width: "20vw",
+            paddingBottom: "7.5vw",
+          }}
+        />
+      </footer>
+    </>
   );
-}
+};
 
 export default App;
